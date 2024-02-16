1a0a0a891601fa64b8ddb2e840989f7b71e66cea
anibalfn <anibalneto29@gmail.com>
2024-02-16 02:52:55 -0300

    gameLabel.ts updated - add interactive input

--- src/app/components/card/card-label/card-label.component.ts
+++ src/app/components/card/card-label/card-label.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card-label',
   templateUrl: './card-label.component.html',
   styleUrl: './card-label.component.css'
 })
-export class CardLabelComponent {
+export class CardLabelComponent implements OnInit {
 
+	@Input()
+	gameLabel: string = ''
+
+	constructor(){}
+
+	ngOnInit(): void {
+
+	}
 }
