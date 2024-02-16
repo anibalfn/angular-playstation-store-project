4435b9c9698f2a6f1e012a3cdbe0300e04a87286
anibalfn <anibalneto29@gmail.com>
2024-02-16 20:48:50 -0300

    Add TS configs in components

--- src/app/components/card/card-price/card-price.component.ts
+++ src/app/components/card/card-price/card-price.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card-price',
   templateUrl: './card-price.component.html',
   styleUrl: './card-price.component.css'
 })
-export class CardPriceComponent {
+export class CardPriceComponent implements OnInit {
+
+	@Input()
+	gameType: string = "DIGITALTEST"
+	@Input()
+	gamePrice: string = "$ 199.99"
+
+
+	constructor() {}
+	ngOnInit(): void {
+
+	}
 
 }
